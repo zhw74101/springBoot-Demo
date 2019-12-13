0bebad6742418a793d5124de59f065c76fd3cd41
zhw <zhw74101@163.com>
2019-12-13 11:58:59 +0800

    springboot入门及配置信息code+文件

---
+++ project/springboot-web/src/main/java/com/springboot/demo/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhanghongwei on 2019/12/13.
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
